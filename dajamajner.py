b39cff2be02419d2fa94c3b21cf21ced8597189d
chelik10228 <dxxeas@gmail.com>
2025-03-20 08:46:11 +0100

    Add files via upload

--- dajamajner.py
+++ dajamajner.py
@@ -6,18 +6,18 @@ print("======================================");
 print("Dajamajner");
 print("======================================");
 time.sleep(1);
-with open("wallet.wal.txt", "w") as f:
+with open("wallet.txt", "w") as f:
 				print();
 
 while (True):
 	money = random.uniform(0, 1000);
 	hex_string = hex(int(money));
-	with open("wallet.wal.txt", "a") as f:
+	with open("wallet.txt", "a") as f:
 
 				print((hex_string), "|", money, "bitfols", "\n", file=f);
 
 	print("======================================");
 	print("\033[33mWallet name: Wallet.Wal\033[0m");
-	print("Wallet size: ", os.path.getsize('wallet.wal.txt'));
+	print("Wallet size: ", os.path.getsize('wallet.txt'));
 	print("\033[32mGiven:", money, "bitfols", "\033[0m");
   
