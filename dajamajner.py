f37eccdcc2664625eb6e5acb3c54c53116f0289f
chelik10228 <dxxeas@gmail.com>
2025-05-14 15:55:16 +0200

    back currency entergit add .!

--- dajamajner.py
+++ dajamajner.py
@@ -5,6 +5,7 @@ import os;
 print("======================================");
 print("Dajamajner");
 print("======================================");
+currency = input("Enter currency: ");
 time.sleep(1);
 with open("wallet.txt", "w") as f:
 				print();
@@ -14,10 +15,10 @@ while (True):
 	hex_string = hex(int(money));
 	with open("wallet.txt", "a") as f:
 
-				print((hex_string), "|", money, "bitfols", "\n", file=f);
+				print((hex_string), "|", money, currency+"s", "\n", file=f);
 
 	print("======================================");
 	print("\033[33mWallet name: Wallet.Wal\033[0m");
 	print("Wallet size: ", os.path.getsize('wallet.txt'));
-	print("\033[32mGiven:", money, "bitfols", "\033[0m");
+	print("\033[32mGiven:", money, currency, "\033[0m");
   
